ab8565b3ab7add2908c6fbae5d15b336817c0cf0
Hamza <hamzii.se@gmail.com>
2023-12-03 21:59:32 +0500

    added chat messages component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -6,6 +6,7 @@ import { db } from "@/lib/db";
 
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 
 interface ChannelIdPageProps {
     params: {
@@ -41,7 +42,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
     return (
         <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
             <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
-            <div className="flex-1">Future messages</div>
+            <ChatMessages
+                member={member}
+                name={channel.name}
+                chatId={channel.id}
+                type="channel"
+                apiUrl="/api/messages"
+                socketUrl="/api/socket/messages"
+                socketQuery={{
+                    serverId: channel.serverId,
+                    channelId: channel.id,
+                }}
+                paramKey="channelId"
+                paramValue={channel.id}
+            />
             <ChatInput
                 name={channel.name}
                 type="channel"
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -43,7 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
                         <SocketProvider>
                             <ModalProvider />
-                            {children}
+                            <QueryProvider>{children}</QueryProvider>
                         </SocketProvider>
                     </ThemeProvider>
                 </body>
