ecaa77ab211abf55327d23c8116c8e7a05005915
Hamza <hamzii.se@gmail.com>
2023-12-04 03:55:35 +0500

    added chat item component with editing functionality

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { Fragment } from "react";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Member, Message, Profile } from "@prisma/client";
 import { useChatQuery } from "@/hooks/use-chat-query";
+import { format } from "date-fns";
 
 import ChatWelcome from "./chat-welcome";
-import { Fragment } from "react";
+import ChatItem from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
     member: Member & {
@@ -70,10 +74,22 @@ const ChatMessages = ({
             <div className="flex-1" />
             <ChatWelcome type={type} name={name} />
             <div className="flex flex-col-reverse mt-auto">
-                {data?.pages.map((group, i) => (
+                {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
                         {group.items.map((message: MessageWithMemberWithProfile) => (
-                            <div key={message.id}>{message.content}</div>
+                            <ChatItem
+                                key={message.id}
+                                id={message.id}
+                                currentMember={member}
+                                member={message.member}
+                                content={message.content}
+                                fileUrl={message.fileUrl}
+                                deleted={message.deleted}
+                                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                isUpdated={message.updatedAt !== message.createdAt}
+                                socketUrl={socketUrl}
+                                socketQuery={socketQuery}
+                            />
                         ))}
                     </Fragment>
                 ))}
